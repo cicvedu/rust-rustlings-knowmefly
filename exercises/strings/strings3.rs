fdcfcc706c5e93de0758b3afef68c0265a4916a6
knowmefly <knowmefly@qq.com>
2023-08-08 22:17:32 +0800

    do  41 test

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,23 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let triminput = input.trim();
+    return triminput.to_string();
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let composed = format!("{} world!", input);
+    return composed;
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let replaced = input.replace("cars", "balloons");
+    replaced
 }
 
 #[cfg(test)]
